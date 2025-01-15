ccb17bfd45b59dd29c18c5f531cf91c2db3bcc65
Gusmiftah12 <gusmif@ilhamxander.web.id>
2025-01-16 03:36:32 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-pip install --no-cache-dir -r requirements.txt
-
-# Jalankan script Python di background dan lepaskan dari shell
-python run.py & disown
-
-echo "Step 1 completed"
+python build.py
